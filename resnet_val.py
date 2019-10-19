ba9f1884f4760efe5e3fbcda8bf06cf5610a3dc4
julianalverio <julian.a.alverio@gmail.com>
2019-10-18 21:59:14 -0400

    iterating

--- resnet_val.py
+++ resnet_val.py
@@ -47,7 +47,7 @@ for class_name in os.listdir(prefix):
     for image_name in os.listdir(prefix + class_name):
         full_path = os.path.join(prefix, class_name, image_name)
         image = Image.open(full_path)
-        image = Image.fromarray(np.array(image))
+        image = torch.tensor(np.array(image))/255.
         import pdb; pdb.set_trace()
         image = normalize(image)
         image = np.array(image)
