a5280783d7b273e768b59b34495150f71b7a0996
julianalverio <julian.a.alverio@gmail.com>
2019-10-19 11:05:04 -0400

    iterating

--- resnet_val.py
+++ resnet_val.py
@@ -50,8 +50,8 @@ for class_name in os.listdir(prefix):
         import pdb; pdb.set_trace()
         image = image.convert('RGB')
         # 3xHxW is expected
-        image = image.permute(2, 0, 1)
         image = torch.tensor(np.array(image))/255.
+        image = image.permute(2, 0, 1)
         image = normalize(image)
         image = np.array(image)
         image = torch.tensor(image).cuda().unsqueeze(0)
