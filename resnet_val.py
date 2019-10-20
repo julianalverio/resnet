b5412bdcaa4dfc4c6a13fe6739b6597ed3109a5c
julianalverio <julian.a.alverio@gmail.com>
2019-10-19 20:12:08 -0400

    iterating

--- resnet_val.py
+++ resnet_val.py
@@ -85,7 +85,7 @@ transformations = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
-        normalize,
+        # normalize,
     ])
 
 prefix = image_dir
@@ -99,8 +99,8 @@ for label_dirname in os.listdir(prefix):
         image = Image.open(full_path)
         import pdb; pdb.set_trace()
         image = transforms.ToPILImage(image)
-        image = transformations(image)
         image = image.convert('RGB')
+        image = transformations(image)
         # image = torch.tensor(np.array(image))/255.
         # image = image.permute(2, 0, 1)         # 3xHxW is expected
         # image = normalize(image)
