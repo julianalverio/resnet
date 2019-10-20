aa66d57b323fd58234a311f5d0485f53336a640a
julianalverio <julian.a.alverio@gmail.com>
2019-10-19 20:11:40 -0400

    iterating

--- resnet_val.py
+++ resnet_val.py
@@ -100,7 +100,7 @@ for label_dirname in os.listdir(prefix):
         import pdb; pdb.set_trace()
         image = transforms.ToPILImage(image)
         image = transformations(image)
-        # image = image.convert('RGB')
+        image = image.convert('RGB')
         # image = torch.tensor(np.array(image))/255.
         # image = image.permute(2, 0, 1)         # 3xHxW is expected
         # image = normalize(image)
