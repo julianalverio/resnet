49b84616ba40d43d2eddcdc10d4a103cd52c9e30
julianalverio <julian.a.alverio@gmail.com>
2019-10-26 16:46:01 -0400

    iterating

--- transfer_learning.py
+++ transfer_learning.py
@@ -58,7 +58,6 @@ class Objectnet(Dataset):
         self.images = []
         success_counter = 0
         for dirname in os.listdir(root):
-            import pdb; pdb.set_trace()
             class_name = dirname.replace('/', '_').replace('-', '_').replace(' ', '_').lower().replace("'", '')
             if class_name not in objectnet2imagenet:
                 continue
