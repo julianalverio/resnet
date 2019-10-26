94d6d525ce260ce7d204f1be44f3ea5ce83f150f
julianalverio <julian.a.alverio@gmail.com>
2019-10-26 15:40:43 -0400

    iterating

--- transfer_learning.py
+++ transfer_learning.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 import torchvision
 import torch
+import torch.nn as nn
 import os
 from PIL import Image
 from torchvision import transforms
@@ -14,6 +15,13 @@ WORKERS = 100
 BATCH_SIZE = 512
 
 model = torchvision.models.resnet152(pretrained=True)
+model.fc = nn.Linear(2048, 1000, bias=True)
+
+import pdb; pdb.set_trace()
+for param in model.parameters():
+    param.requires_grad = False
+
+
 import pdb; pdb.set_trace()
 # model = model.eval().to(DEVICE)
 # model = nn.DataParallel(model)
