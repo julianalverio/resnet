6230e864938972bb6e0e21ce7424504da98f22a3
julianalverio <julian.a.alverio@gmail.com>
2019-10-27 00:34:34 -0400

    iterating

--- transfer_learning.py
+++ transfer_learning.py
@@ -110,6 +110,7 @@ for class_int in all_classes:
 optimizer = Adam(model.parameters(), lr=0.0003)
 all_batches = []
 import pdb; pdb.set_trace()
+first_batch = next(val_loader)
 for batch_counter, (batch, labels) in enumerate(val_loader):
     valid_idxs = []
     labels = labels[0]
