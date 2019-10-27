556de885fcbadcba52a69f675a7a04700f02edd0
julianalverio <julian.a.alverio@gmail.com>
2019-10-27 07:39:05 -0400

    iterating

--- transfer_learning.py
+++ transfer_learning.py
@@ -11,9 +11,11 @@ import json
 import pickle
 from torch.optim import Adam
 import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', type=int)
+parser.add_argument('--overlap', action='store_true')
 args = parser.parse_args()
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -37,15 +39,16 @@ with open('/storage/jalverio/resnet/dirname_to_objectnet_name.json') as f:
 
 
 class Objectnet(Dataset):
-    def __init__(self, root, transform, objectnet2torch, num_examples, test):
+    def __init__(self, root, transform, objectnet2torch, num_examples, test, overlap):
         self.root = root
         self.transform = transform
         self.images = []
         classes_in_dataset = set()
         for dirname in os.listdir(root):
             class_name = dirname_to_classname[dirname]
-            if class_name not in objectnet2torch:
-                continue
+            if overlap:
+                if class_name not in objectnet2torch:
+                    continue
             classes_in_dataset.add(class_name)
             labels = objectnet2torch[class_name]
             images = os.listdir(os.path.join(root, dirname))
@@ -54,6 +57,8 @@ class Objectnet(Dataset):
                 self.images.append((path, labels))
         print('Created objectnet dataset with %s classes' % len(classes_in_dataset))
         self.n_per_class(num_examples, test)
+
+        self.classes_in_dataset = classes_in_dataset
 
     def n_per_class(self, num_examples, test):
         valid_classes = set()
@@ -90,58 +95,8 @@ class Objectnet(Dataset):
         return len(self.images)
 
 
-# ## THIS IS FOR THE TEST SET
-# class Objectnet2(Dataset):
-#     def __init__(self, root, transform, objectnet2torch):
-#         self.root = root
-#         self.transform = transform
-#         self.images = []
-#         classes_in_dataset = set()
-#         for dirname in os.listdir(root):
-#             class_name = dirname_to_classname[dirname]
-#             if class_name not in objectnet2torch:
-#                 continue
-#             classes_in_dataset.add(class_name)
-#             labels = objectnet2torch[class_name]
-#             images = os.listdir(os.path.join(root, dirname))
-#             for image_name in images:
-#                 path = os.path.join(root, dirname, image_name)
-#                 self.images.append((path, labels))
-#         print('Created objectnet dataset with %s classes' % len(classes_in_dataset))
-#
-#     def n_per_class(self, num_examples):
-#         valid_classes = set()
-#         for _, label_list in self.images:
-#             for label in label_list:
-#                 valid_classes.add(torch2objectnet[label])
-#
-#         quotas = dict()
-#         for label in valid_classes:
-#             quotas[label] = 0
-#         remaining_images = []
-#         for path, label_list in self.images:
-#             objectnet_label = torch2objectnet[label_list[0]]
-#             if quotas[objectnet_label] < num_examples * 2:
-#                 if quotas[objectnet_label] >= num_examples:
-#                     remaining_images.append((path, label_list))
-#                 quotas[objectnet_label] += 1
-#         self.images = remaining_images
-#         print('Purged some examples. %s classes and %s examples remaining.' % (len(valid_classes), len(self.images)))
-#
-#     def __getitem__(self, index):
-#         full_path, labels = self.images[index]
-#         image = Image.open(full_path)
-#         image = image.convert('RGB')
-#         image = self.transform(image)
-#         return image, labels
-#
-#     def __len__(self):
-#         return len(self.images)
-
-
 def accuracy_objectnet(output, target):
     with torch.no_grad():
-        # pred is n x 5
         _, pred = output.topk(5, 1, True, True)
     top5_correct = 0
     top1_correct = 0
@@ -154,13 +109,43 @@ def accuracy_objectnet(output, target):
 
         if prediction[0].item() in target_set:
             top1_correct += 1
-
     return top1_correct, top5_correct
+
+
+def accuracy_objectnet_nobatch(output, target):
+    _, pred = output.topk(5, 1, True, True)
+    pred_set = set(output.cpu().numpy().tolist())
+    # top 1 succeeded
+    if output[0].item() == target.item():
+        return np.ones((2,))
+    # top 5 succeeded
+    if target.item() in pred_set:
+        return np.array([0, 1])
+    # neither succeeded
+    return np.zeros((2,))
+
+
+class Saver(object):
+    def __init__(self, n_examples, num_classes):
+        self.records = []
+        self.n_examples = n_examples
+        self.num_classes = num_classes
+
+    def write_record(self, record):
+        self.records.append(record)
+
+    def write_to_disk(self):
+        name = '%s_examples_%s_classes_%s_epochs' % (self.n_examples, self.num_classes, len(self.records))
+        with open('/storage/jalverio/resnet/' + name, 'wb') as f:
+            pickle.dump(self.records, f)
+        print('The saver has saved!')
+
 
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 WORKERS = 50
 BATCH_SIZE = 32
+
 
 model = torchvision.models.resnet152(pretrained=True).eval()
 for param in model.parameters():
@@ -170,11 +155,13 @@ model = model.eval().to(DEVICE)
 model = nn.DataParallel(model)
 
 N_EXAMPLES = args.n
+OVERLAP = args.overlap
 
 
 image_dir = '/storage/jalverio/objectnet-oct-24-d123/'
-dataset = Objectnet(image_dir, transformations, objectnet2torch, N_EXAMPLES, test=False)
-dataset_test = Objectnet(image_dir, transformations, objectnet2torch, N_EXAMPLES, test=True)
+dataset = Objectnet(image_dir, transformations, objectnet2torch, N_EXAMPLES, test=False, overlap=OVERLAP)
+total_classes = len(dataset.classes_in_dataset)
+dataset_test = Objectnet(image_dir, transformations, objectnet2torch, N_EXAMPLES, test=True, overlap=OVERLAP)
 val_loader = torch.utils.data.DataLoader(
         dataset,
         batch_size=BATCH_SIZE, shuffle=False,
@@ -182,23 +169,37 @@ val_loader = torch.utils.data.DataLoader(
 
 test_loader = torch.utils.data.DataLoader(
         dataset_test,
-        batch_size=BATCH_SIZE, shuffle=False,
+        batch_size=1, shuffle=False,
         num_workers=WORKERS, pin_memory=True)
 
+SAVER = Saver(N_EXAMPLES, total_classes)
 
+
+valid_classes = set()
+for _, label_list in dataset.images:
+    for label in label_list:
+        valid_classes.add(torch2objectnet[label])
+TORCH_CLASSES = valid_classes
+
+
+# THIS DOES NOT USE BATCHING TO ALLOW FOR BETTER LOGGING
 def evaluate():
-    total_top1, total_top5, total_examples = 0, 0, 0
-    for batch_counter, (batch, labels) in enumerate(test_loader):
+    total_top1, total_top5 = 0, 0
+    score_dict = dict()
+    for torch_class in TORCH_CLASSES:
+        score_dict[torch_class] = np.zeros((2,))
+    for batch, labels in test_loader:
         labels = labels[0].to(DEVICE)
         batch = batch.to(DEVICE)
         logits = model(batch)
-        top1, top5 = accuracy_objectnet(logits, labels)
-        total_top1 += top1
-        total_top5 += top5
-        total_examples += batch.shape[0]
+        accuracy_results = accuracy_objectnet_nobatch(logits, labels)
+        score_dict[labels[0].item()] += accuracy_results
+        total_top1 += accuracy_results[0]
+        total_top5 += accuracy_results[1]
+    total_examples = len(test_loader)
     top1_score = total_top1 / total_examples
     top5_score = total_top5 / total_examples
-    print(total_examples)
+    SAVER.write_record(score_dict, total_examples, N_EXAMPLES)
     return top1_score, top5_score
 
 
@@ -210,6 +211,7 @@ total_top1, total_top5, total_examples = 0, 0, 0
 for epoch in range(50):
     total_examples = 0
     print('starting epoch %s' % epoch)
+    import pdb; pdb.set_trace()
     for batch_counter, (batch, labels) in enumerate(val_loader):
         labels = labels[0].to(DEVICE)
         batch = batch.to(DEVICE)
@@ -225,11 +227,7 @@ for epoch in range(50):
         top_score = current_accuracy
     print('top1 score: %s' % top1_score)
     print('top5 score: %s' % current_accuracy)
-    # diff = abs(previous_accuracy) - abs(current_accuracy)
-    # if diff < 0.005 and epoch >= 20:
-    #     print('breaking out')
-    #     break
-    # previous_loss = loss
 
+SAVER.write_to_disk()
 print('BEST top5', top_score)
 
