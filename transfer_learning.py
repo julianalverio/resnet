bc9addc952a5a9b663f603943950e479cce7e2d0
julianalverio <julian.a.alverio@gmail.com>
2019-10-27 08:35:40 -0400

    iterating

--- transfer_learning.py
+++ transfer_learning.py
@@ -214,7 +214,7 @@ for epoch in range(50):
     print('starting epoch %s' % epoch)
     for batch_counter, (batch, labels) in enumerate(val_loader):
         import pdb; pdb.set_trace()
-        labels = labels[0].to(DEVICE)
+        labels = labels.to(DEVICE)
         batch = batch.to(DEVICE)
         logits = model(batch)
         loss = criterion(logits, labels)
